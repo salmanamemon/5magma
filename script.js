e2db24262f597b11c619355711a8189e815496ec
Salman A Memon <memon.salman@gmail.com>
2024-09-20 22:15:48 +0400

    Reverted to initial

--- script.js
+++ script.js
@@ -70,72 +70,72 @@ window.addEventListener("resize", function () {
 
 function files(index) {
   var data = `
-  ./images/frames/frames00007.png
-  ./images/frames/frames00010.png
-  ./images/frames/frames00013.png
-  ./images/frames/frames00016.png
-  ./images/frames/frames00019.png
-  ./images/frames/frames00022.png
-  ./images/frames/frames00025.png
-  ./images/frames/frames00028.png
-  ./images/frames/frames00031.png
-  ./images/frames/frames00034.png
-  ./images/frames/frames00037.png
-  ./images/frames/frames00040.png
-  ./images/frames/frames00043.png
-  ./images/frames/frames00046.png
-  ./images/frames/frames00049.png
-  ./images/frames/frames00052.png
-  ./images/frames/frames00055.png
-  ./images/frames/frames00058.png
-  ./images/frames/frames00061.png
-  ./images/frames/frames00064.png
-  ./images/frames/frames00067.png
-  ./images/frames/frames00070.png
-  ./images/frames/frames00073.png
-  ./images/frames/frames00076.png
-  ./images/frames/frames00079.png
-  ./images/frames/frames00082.png
-  ./images/frames/frames00085.png
-  ./images/frames/frames00088.png
-  ./images/frames/frames00091.png
-  ./images/frames/frames00094.png
-  ./images/frames/frames00097.png
-  ./images/frames/frames00100.png
-  ./images/frames/frames00103.png
-  ./images/frames/frames00106.png
-  ./images/frames/frames00109.png
-  ./images/frames/frames00112.png
-  ./images/frames/frames00115.png
-  ./images/frames/frames00118.png
-  ./images/frames/frames00121.png
-  ./images/frames/frames00124.png
-  ./images/frames/frames00127.png
-  ./images/frames/frames00130.png
-  ./images/frames/frames00133.png
-  ./images/frames/frames00136.png
-  ./images/frames/frames00139.png
-  ./images/frames/frames00142.png
-  ./images/frames/frames00145.png
-  ./images/frames/frames00148.png
-  ./images/frames/frames00151.png
-  ./images/frames/frames00154.png
-  ./images/frames/frames00157.png
-  ./images/frames/frames00160.png
-  ./images/frames/frames00163.png
-  ./images/frames/frames00166.png
-  ./images/frames/frames00169.png
-  ./images/frames/frames00172.png
-  ./images/frames/frames00175.png
-  ./images/frames/frames00178.png
-  ./images/frames/frames00181.png
-  ./images/frames/frames00184.png
-  ./images/frames/frames00187.png
-  ./images/frames/frames00190.png
-  ./images/frames/frames00193.png
-  ./images/frames/frames00196.png
-  ./images/frames/frames00199.png
-  ./images/frames/frames00202.png
+  ./frames00007.png
+  ./frames00010.png
+  ./frames00013.png
+  ./frames00016.png
+  ./frames00019.png
+  ./frames00022.png
+  ./frames00025.png
+  ./frames00028.png
+  ./frames00031.png
+  ./frames00034.png
+  ./frames00037.png
+  ./frames00040.png
+  ./frames00043.png
+  ./frames00046.png
+  ./frames00049.png
+  ./frames00052.png
+  ./frames00055.png
+  ./frames00058.png
+  ./frames00061.png
+  ./frames00064.png
+  ./frames00067.png
+  ./frames00070.png
+  ./frames00073.png
+  ./frames00076.png
+  ./frames00079.png
+  ./frames00082.png
+  ./frames00085.png
+  ./frames00088.png
+  ./frames00091.png
+  ./frames00094.png
+  ./frames00097.png
+  ./frames00100.png
+  ./frames00103.png
+  ./frames00106.png
+  ./frames00109.png
+  ./frames00112.png
+  ./frames00115.png
+  ./frames00118.png
+  ./frames00121.png
+  ./frames00124.png
+  ./frames00127.png
+  ./frames00130.png
+  ./frames00133.png
+  ./frames00136.png
+  ./frames00139.png
+  ./frames00142.png
+  ./frames00145.png
+  ./frames00148.png
+  ./frames00151.png
+  ./frames00154.png
+  ./frames00157.png
+  ./frames00160.png
+  ./frames00163.png
+  ./frames00166.png
+  ./frames00169.png
+  ./frames00172.png
+  ./frames00175.png
+  ./frames00178.png
+  ./frames00181.png
+  ./frames00184.png
+  ./frames00187.png
+  ./frames00190.png
+  ./frames00193.png
+  ./frames00196.png
+  ./frames00199.png
+  ./frames00202.png
  `;
   return data.split("\n")[index];
 }
@@ -251,73 +251,73 @@ render();
 
 function files(index) {
 var data = `
-./images/bridges/bridges00004.png
-./images/bridges/bridges00007.png
-./images/bridges/bridges00010.png
-./images/bridges/bridges00013.png
-./images/bridges/bridges00016.png
-./images/bridges/bridges00019.png
-./images/bridges/bridges00022.png
-./images/bridges/bridges00025.png
-./images/bridges/bridges00028.png
-./images/bridges/bridges00031.png
-./images/bridges/bridges00034.png
-./images/bridges/bridges00037.png
-./images/bridges/bridges00040.png
-./images/bridges/bridges00043.png
-./images/bridges/bridges00046.png
-./images/bridges/bridges00049.png
-./images/bridges/bridges00052.png
-./images/bridges/bridges00055.png
-./images/bridges/bridges00058.png
-./images/bridges/bridges00061.png
-./images/bridges/bridges00064.png
-./images/bridges/bridges00067.png
-./images/bridges/bridges00070.png
-./images/bridges/bridges00073.png
-./images/bridges/bridges00076.png
-./images/bridges/bridges00079.png
-./images/bridges/bridges00082.png
-./images/bridges/bridges00085.png
-./images/bridges/bridges00088.png
-./images/bridges/bridges00091.png
-./images/bridges/bridges00094.png
-./images/bridges/bridges00097.png
-./images/bridges/bridges00100.png
-./images/bridges/bridges00103.png
-./images/bridges/bridges00106.png
-./images/bridges/bridges00109.png
-./images/bridges/bridges00112.png
-./images/bridges/bridges00115.png
-./images/bridges/bridges00118.png
-./images/bridges/bridges00121.png
-./images/bridges/bridges00124.png
-./images/bridges/bridges00127.png
-./images/bridges/bridges00130.png
-./images/bridges/bridges00133.png
-./images/bridges/bridges00136.png
-./images/bridges/bridges00139.png
-./images/bridges/bridges00142.png
-./images/bridges/bridges00145.png
-./images/bridges/bridges00148.png
-./images/bridges/bridges00151.png
-./images/bridges/bridges00154.png
-./images/bridges/bridges00157.png
-./images/bridges/bridges00160.png
-./images/bridges/bridges00163.png
-./images/bridges/bridges00166.png
-./images/bridges/bridges00169.png
-./images/bridges/bridges00172.png
-./images/bridges/bridges00175.png
-./images/bridges/bridges00178.png
-./images/bridges/bridges00181.png
-./images/bridges/bridges00184.png
-./images/bridges/bridges00187.png
-./images/bridges/bridges00190.png
-./images/bridges/bridges00193.png
-./images/bridges/bridges00196.png
-./images/bridges/bridges00199.png
-./images/bridges/bridges00202.png
+./bridges00004.png
+./bridges00007.png
+./bridges00010.png
+./bridges00013.png
+./bridges00016.png
+./bridges00019.png
+./bridges00022.png
+./bridges00025.png
+./bridges00028.png
+./bridges00031.png
+./bridges00034.png
+./bridges00037.png
+./bridges00040.png
+./bridges00043.png
+./bridges00046.png
+./bridges00049.png
+./bridges00052.png
+./bridges00055.png
+./bridges00058.png
+./bridges00061.png
+./bridges00064.png
+./bridges00067.png
+./bridges00070.png
+./bridges00073.png
+./bridges00076.png
+./bridges00079.png
+./bridges00082.png
+./bridges00085.png
+./bridges00088.png
+./bridges00091.png
+./bridges00094.png
+./bridges00097.png
+./bridges00100.png
+./bridges00103.png
+./bridges00106.png
+./bridges00109.png
+./bridges00112.png
+./bridges00115.png
+./bridges00118.png
+./bridges00121.png
+./bridges00124.png
+./bridges00127.png
+./bridges00130.png
+./bridges00133.png
+./bridges00136.png
+./bridges00139.png
+./bridges00142.png
+./bridges00145.png
+./bridges00148.png
+./bridges00151.png
+./bridges00154.png
+./bridges00157.png
+./bridges00160.png
+./bridges00163.png
+./bridges00166.png
+./bridges00169.png
+./bridges00172.png
+./bridges00175.png
+./bridges00178.png
+./bridges00181.png
+./bridges00184.png
+./bridges00187.png
+./bridges00190.png
+./bridges00193.png
+./bridges00196.png
+./bridges00199.png
+./bridges00202.png
 `;
 return data.split("\n")[index];
 }
@@ -431,142 +431,142 @@ render();
 function files(index) {
 var data = `
 
-./images/bookpages/1.webp?2
-./images/bookpages/2.webp?2
-./images/bookpages/3.webp?2
-./images/bookpages/4.webp?2
-./images/bookpages/5.webp?2
-./images/bookpages/6.webp?2
-./images/bookpages/7.webp?2
-./images/bookpages/8.webp?2
-./images/bookpages/9.webp?2
-./images/bookpages/10.webp?2
-./images/bookpages/11.webp?2
-./images/bookpages/12.webp?2
-./images/bookpages/13.webp?2
-./images/bookpages/14.webp?2
-./images/bookpages/15.webp?2
-./images/bookpages/16.webp?2
-./images/bookpages/17.webp?2
-./images/bookpages/18.webp?2
-./images/bookpages/19.webp?2
-./images/bookpages/20.webp?2
-./images/bookpages/21.webp?2
-./images/bookpages/22.webp?2
-./images/bookpages/23.webp?2
-./images/bookpages/24.webp?2
-./images/bookpages/25.webp?2
-./images/bookpages/26.webp?2
-./images/bookpages/27.webp?2
-./images/bookpages/28.webp?2
-./images/bookpages/29.webp?2
-./images/bookpages/30.webp?2
-./images/bookpages/31.webp?2
-./images/bookpages/32.webp?2
-./images/bookpages/33.webp?2
-./images/bookpages/34.webp?2
-./images/bookpages/35.webp?2
-./images/bookpages/36.webp?2
-./images/bookpages/37.webp?2
-./images/bookpages/38.webp?2
-./images/bookpages/39.webp?2
-./images/bookpages/40.webp?2
-./images/bookpages/41.webp?2
-./images/bookpages/42.webp?2
-./images/bookpages/43.webp?2
-./images/bookpages/44.webp?2
-./images/bookpages/45.webp?2
-./images/bookpages/46.webp?2
-./images/bookpages/47.webp?2
-./images/bookpages/48.webp?2
-./images/bookpages/49.webp?2
-./images/bookpages/50.webp?2
-./images/bookpages/51.webp?2
-./images/bookpages/52.webp?2
-./images/bookpages/53.webp?2
-./images/bookpages/54.webp?2
-./images/bookpages/55.webp?2
-./images/bookpages/56.webp?2
-./images/bookpages/57.webp?2
-./images/bookpages/58.webp?2
-./images/bookpages/59.webp?2
-./images/bookpages/60.webp?2
-./images/bookpages/61.webp?2
-./images/bookpages/62.webp?2
-./images/bookpages/63.webp?2
-./images/bookpages/64.webp?2
-./images/bookpages/65.webp?2
-./images/bookpages/66.webp?2
-./images/bookpages/67.webp?2
-./images/bookpages/68.webp?2
-./images/bookpages/69.webp?2
-./images/bookpages/70.webp?2
-./images/bookpages/71.webp?2
-./images/bookpages/72.webp?2
-./images/bookpages/73.webp?2
-./images/bookpages/74.webp?2
-./images/bookpages/75.webp?2
-./images/bookpages/76.webp?2
-./images/bookpages/77.webp?2
-./images/bookpages/78.webp?2
-./images/bookpages/79.webp?2
-./images/bookpages/80.webp?2
-./images/bookpages/81.webp?2
-./images/bookpages/82.webp?2
-./images/bookpages/83.webp?2
-./images/bookpages/84.webp?2
-./images/bookpages/85.webp?2
-./images/bookpages/86.webp?2
-./images/bookpages/87.webp?2
-./images/bookpages/88.webp?2
-./images/bookpages/89.webp?2
-./images/bookpages/90.webp?2
-./images/bookpages/91.webp?2
-./images/bookpages/92.webp?2
-./images/bookpages/93.webp?2
-./images/bookpages/94.webp?2
-./images/bookpages/95.webp?2
-./images/bookpages/96.webp?2
-./images/bookpages/97.webp?2
-./images/bookpages/98.webp?2
-./images/bookpages/99.webp?2
-./images/bookpages/100.webp?2
-./images/bookpages/101.webp?2
-./images/bookpages/102.webp?2
-./images/bookpages/103.webp?2
-./images/bookpages/104.webp?2
-./images/bookpages/105.webp?2
-./images/bookpages/106.webp?2
-./images/bookpages/107.webp?2
-./images/bookpages/108.webp?2
-./images/bookpages/109.webp?2
-./images/bookpages/110.webp?2
-./images/bookpages/111.webp?2
-./images/bookpages/112.webp?2
-./images/bookpages/113.webp?2
-./images/bookpages/114.webp?2
-./images/bookpages/115.webp?2
-./images/bookpages/116.webp?2
-./images/bookpages/117.webp?2
-./images/bookpages/118.webp?2
-./images/bookpages/119.webp?2
-./images/bookpages/120.webp?2
-./images/bookpages/121.webp?2
-./images/bookpages/122.webp?2
-./images/bookpages/123.webp?2
-./images/bookpages/124.webp?2
-./images/bookpages/125.webp?2
-./images/bookpages/126.webp?2
-./images/bookpages/127.webp?2
-./images/bookpages/128.webp?2
-./images/bookpages/129.webp?2
-./images/bookpages/130.webp?2
-./images/bookpages/131.webp?2
-./images/bookpages/132.webp?2
-./images/bookpages/133.webp?2
-./images/bookpages/134.webp?2
-./images/bookpages/135.webp?2
-./images/bookpages/136.webp?2
+https://thisismagma.com/assets/home/lore/seq/1.webp?2
+https://thisismagma.com/assets/home/lore/seq/2.webp?2
+https://thisismagma.com/assets/home/lore/seq/3.webp?2
+https://thisismagma.com/assets/home/lore/seq/4.webp?2
+https://thisismagma.com/assets/home/lore/seq/5.webp?2
+https://thisismagma.com/assets/home/lore/seq/6.webp?2
+https://thisismagma.com/assets/home/lore/seq/7.webp?2
+https://thisismagma.com/assets/home/lore/seq/8.webp?2
+https://thisismagma.com/assets/home/lore/seq/9.webp?2
+https://thisismagma.com/assets/home/lore/seq/10.webp?2
+https://thisismagma.com/assets/home/lore/seq/11.webp?2
+https://thisismagma.com/assets/home/lore/seq/12.webp?2
+https://thisismagma.com/assets/home/lore/seq/13.webp?2
+https://thisismagma.com/assets/home/lore/seq/14.webp?2
+https://thisismagma.com/assets/home/lore/seq/15.webp?2
+https://thisismagma.com/assets/home/lore/seq/16.webp?2
+https://thisismagma.com/assets/home/lore/seq/17.webp?2
+https://thisismagma.com/assets/home/lore/seq/18.webp?2
+https://thisismagma.com/assets/home/lore/seq/19.webp?2
+https://thisismagma.com/assets/home/lore/seq/20.webp?2
+https://thisismagma.com/assets/home/lore/seq/21.webp?2
+https://thisismagma.com/assets/home/lore/seq/22.webp?2
+https://thisismagma.com/assets/home/lore/seq/23.webp?2
+https://thisismagma.com/assets/home/lore/seq/24.webp?2
+https://thisismagma.com/assets/home/lore/seq/25.webp?2
+https://thisismagma.com/assets/home/lore/seq/26.webp?2
+https://thisismagma.com/assets/home/lore/seq/27.webp?2
+https://thisismagma.com/assets/home/lore/seq/28.webp?2
+https://thisismagma.com/assets/home/lore/seq/29.webp?2
+https://thisismagma.com/assets/home/lore/seq/30.webp?2
+https://thisismagma.com/assets/home/lore/seq/31.webp?2
+https://thisismagma.com/assets/home/lore/seq/32.webp?2
+https://thisismagma.com/assets/home/lore/seq/33.webp?2
+https://thisismagma.com/assets/home/lore/seq/34.webp?2
+https://thisismagma.com/assets/home/lore/seq/35.webp?2
+https://thisismagma.com/assets/home/lore/seq/36.webp?2
+https://thisismagma.com/assets/home/lore/seq/37.webp?2
+https://thisismagma.com/assets/home/lore/seq/38.webp?2
+https://thisismagma.com/assets/home/lore/seq/39.webp?2
+https://thisismagma.com/assets/home/lore/seq/40.webp?2
+https://thisismagma.com/assets/home/lore/seq/41.webp?2
+https://thisismagma.com/assets/home/lore/seq/42.webp?2
+https://thisismagma.com/assets/home/lore/seq/43.webp?2
+https://thisismagma.com/assets/home/lore/seq/44.webp?2
+https://thisismagma.com/assets/home/lore/seq/45.webp?2
+https://thisismagma.com/assets/home/lore/seq/46.webp?2
+https://thisismagma.com/assets/home/lore/seq/47.webp?2
+https://thisismagma.com/assets/home/lore/seq/48.webp?2
+https://thisismagma.com/assets/home/lore/seq/49.webp?2
+https://thisismagma.com/assets/home/lore/seq/50.webp?2
+https://thisismagma.com/assets/home/lore/seq/51.webp?2
+https://thisismagma.com/assets/home/lore/seq/52.webp?2
+https://thisismagma.com/assets/home/lore/seq/53.webp?2
+https://thisismagma.com/assets/home/lore/seq/54.webp?2
+https://thisismagma.com/assets/home/lore/seq/55.webp?2
+https://thisismagma.com/assets/home/lore/seq/56.webp?2
+https://thisismagma.com/assets/home/lore/seq/57.webp?2
+https://thisismagma.com/assets/home/lore/seq/58.webp?2
+https://thisismagma.com/assets/home/lore/seq/59.webp?2
+https://thisismagma.com/assets/home/lore/seq/60.webp?2
+https://thisismagma.com/assets/home/lore/seq/61.webp?2
+https://thisismagma.com/assets/home/lore/seq/62.webp?2
+https://thisismagma.com/assets/home/lore/seq/63.webp?2
+https://thisismagma.com/assets/home/lore/seq/64.webp?2
+https://thisismagma.com/assets/home/lore/seq/65.webp?2
+https://thisismagma.com/assets/home/lore/seq/66.webp?2
+https://thisismagma.com/assets/home/lore/seq/67.webp?2
+https://thisismagma.com/assets/home/lore/seq/68.webp?2
+https://thisismagma.com/assets/home/lore/seq/69.webp?2
+https://thisismagma.com/assets/home/lore/seq/70.webp?2
+https://thisismagma.com/assets/home/lore/seq/71.webp?2
+https://thisismagma.com/assets/home/lore/seq/72.webp?2
+https://thisismagma.com/assets/home/lore/seq/73.webp?2
+https://thisismagma.com/assets/home/lore/seq/74.webp?2
+https://thisismagma.com/assets/home/lore/seq/75.webp?2
+https://thisismagma.com/assets/home/lore/seq/76.webp?2
+https://thisismagma.com/assets/home/lore/seq/77.webp?2
+https://thisismagma.com/assets/home/lore/seq/78.webp?2
+https://thisismagma.com/assets/home/lore/seq/79.webp?2
+https://thisismagma.com/assets/home/lore/seq/80.webp?2
+https://thisismagma.com/assets/home/lore/seq/81.webp?2
+https://thisismagma.com/assets/home/lore/seq/82.webp?2
+https://thisismagma.com/assets/home/lore/seq/83.webp?2
+https://thisismagma.com/assets/home/lore/seq/84.webp?2
+https://thisismagma.com/assets/home/lore/seq/85.webp?2
+https://thisismagma.com/assets/home/lore/seq/86.webp?2
+https://thisismagma.com/assets/home/lore/seq/87.webp?2
+https://thisismagma.com/assets/home/lore/seq/88.webp?2
+https://thisismagma.com/assets/home/lore/seq/89.webp?2
+https://thisismagma.com/assets/home/lore/seq/90.webp?2
+https://thisismagma.com/assets/home/lore/seq/91.webp?2
+https://thisismagma.com/assets/home/lore/seq/92.webp?2
+https://thisismagma.com/assets/home/lore/seq/93.webp?2
+https://thisismagma.com/assets/home/lore/seq/94.webp?2
+https://thisismagma.com/assets/home/lore/seq/95.webp?2
+https://thisismagma.com/assets/home/lore/seq/96.webp?2
+https://thisismagma.com/assets/home/lore/seq/97.webp?2
+https://thisismagma.com/assets/home/lore/seq/98.webp?2
+https://thisismagma.com/assets/home/lore/seq/99.webp?2
+https://thisismagma.com/assets/home/lore/seq/100.webp?2
+https://thisismagma.com/assets/home/lore/seq/101.webp?2
+https://thisismagma.com/assets/home/lore/seq/102.webp?2
+https://thisismagma.com/assets/home/lore/seq/103.webp?2
+https://thisismagma.com/assets/home/lore/seq/104.webp?2
+https://thisismagma.com/assets/home/lore/seq/105.webp?2
+https://thisismagma.com/assets/home/lore/seq/106.webp?2
+https://thisismagma.com/assets/home/lore/seq/107.webp?2
+https://thisismagma.com/assets/home/lore/seq/108.webp?2
+https://thisismagma.com/assets/home/lore/seq/109.webp?2
+https://thisismagma.com/assets/home/lore/seq/110.webp?2
+https://thisismagma.com/assets/home/lore/seq/111.webp?2
+https://thisismagma.com/assets/home/lore/seq/112.webp?2
+https://thisismagma.com/assets/home/lore/seq/113.webp?2
+https://thisismagma.com/assets/home/lore/seq/114.webp?2
+https://thisismagma.com/assets/home/lore/seq/115.webp?2
+https://thisismagma.com/assets/home/lore/seq/116.webp?2
+https://thisismagma.com/assets/home/lore/seq/117.webp?2
+https://thisismagma.com/assets/home/lore/seq/118.webp?2
+https://thisismagma.com/assets/home/lore/seq/119.webp?2
+https://thisismagma.com/assets/home/lore/seq/120.webp?2
+https://thisismagma.com/assets/home/lore/seq/121.webp?2
+https://thisismagma.com/assets/home/lore/seq/122.webp?2
+https://thisismagma.com/assets/home/lore/seq/123.webp?2
+https://thisismagma.com/assets/home/lore/seq/124.webp?2
+https://thisismagma.com/assets/home/lore/seq/125.webp?2
+https://thisismagma.com/assets/home/lore/seq/126.webp?2
+https://thisismagma.com/assets/home/lore/seq/127.webp?2
+https://thisismagma.com/assets/home/lore/seq/128.webp?2
+https://thisismagma.com/assets/home/lore/seq/129.webp?2
+https://thisismagma.com/assets/home/lore/seq/130.webp?2
+https://thisismagma.com/assets/home/lore/seq/131.webp?2
+https://thisismagma.com/assets/home/lore/seq/132.webp?2
+https://thisismagma.com/assets/home/lore/seq/133.webp?2
+https://thisismagma.com/assets/home/lore/seq/134.webp?2
+https://thisismagma.com/assets/home/lore/seq/135.webp?2
+https://thisismagma.com/assets/home/lore/seq/136.webp?2
 
 `;
 return data.split("\n")[index];
